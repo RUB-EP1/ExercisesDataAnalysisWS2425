3bc9e80d04bf1b58c307771b24147d3c9be240e2
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2025-01-21 20:40:03 +0100

    add lecture 13 with refs

---
+++ lectures/lecture-13-a.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.19.47
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2a98d7d6-d82e-11ef-332c-8b360da20e2b
+md"""
+# Lecture 13a: Systematic uncertainties
+
+Discussion followed **Chapter 8** of Data Analysis in HEP.
+
+Also we discussed several examples:
+- History plots in PDF, ([web link](https://pdg.lbl.gov/2024/reviews/contents_sports.html))
+- Radiative decays of X ([LHCb paper](https://inspirehep.net/literature/2801947))
+"""
+
+# ╔═╡ Cell order:
+# ╟─2a98d7d6-d82e-11ef-332c-8b360da20e2b
